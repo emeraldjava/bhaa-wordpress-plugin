b88bec854460d06d5c5951247477421b8352f92e
paul.oconnell <paul.o'connell@mastercard.com>
2018-03-17 22:16:39 +0000

    Add basic Runner and Connections classes.

---
+++ src/front/connections/Connections.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: e074820
+ * Date: 17/03/2018
+ * Time: 21:04
+ */
+
+namespace BHAA\front\connections;
+
+class Connections {
+    const EVENT_TO_RACE = 'event_to_race';
+    const LEAGUE_TO_EVENT = 'league_to_event';
+    const HOUSE_TO_RUNNER = 'house_to_runner';
+    const SECTORTEAM_TO_RUNNER = 'sectorteam_to_runner';
+    const TEAM_CONTACT = 'team_contact';
+    // indicates a runner who will get 10 league points for organsing a race
+    const RACE_ORGANISER = 'race_organiser';
+    // indicates a team that 6 leagues points for organising an event
+    const TEAM_POINTS = 'team_points';
+}
---
+++ src/front/runner/Runner.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: e074820
+ * Date: 17/03/2018
+ * Time: 21:04
+ */
+
+namespace BHAA\front\runner;
+
+
+class Runner {
+
+    const BHAA_RUNNER_DATEOFBIRTH = 'bhaa_runner_dateofbirth';
+    const BHAA_RUNNER_COMPANY = 'bhaa_runner_company';
+    const BHAA_RUNNER_MOBILEPHONE = 'bhaa_runner_mobilephone';
+    const BHAA_RUNNER_INSERTDATE = 'bhaa_runner_insertdate';
+    const BHAA_RUNNER_DATEOFRENEWAL = 'bhaa_runner_dateofrenewal';
+    const BHAA_RUNNER_TERMS_AND_CONDITIONS = 'bhaa_runner_terms_and_conditions';
+
+    const BHAA_RUNNER_STATUS = 'bhaa_runner_status';
+    const MEMBER = 'M';
+    const INACTIVE = 'I';
+    const DAY = 'D';
+
+    const BHAA_RUNNER_GENDER = 'bhaa_runner_gender';
+    const MAN = 'M';
+    const WOMAN = 'W';
+
+    const BHAA_RUNNER_STANDARD = 'bhaa_runner_standard';
+}
