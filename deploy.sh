a973631c3e24a7cb9686cb01aedd19647488f16f
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-14 16:34:03 +0100

    Update the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,9 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
 fi
 
 echo 'FTP_USER     '$1
+echo 'FTP_PASSWORD '$2
+echo 'FTP_SITE     '$3
+echo 'FTP_PATH     '$4
+echo ftp://$1:$2@$3/$4
+curl --ftp-create-dirs -u "$1:$2" -T README.md                               ftp://$3/$4/README.md
+curl --list-only -u "$1:$2" ftp://$3/$4/
