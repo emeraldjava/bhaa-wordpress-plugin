e0b994f34b2a38a8eddb5940b95d6f185565d791
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-16 15:15:15 +0100

    Results shortcode with event, races and league.

--- src/core/race/partials/list.race.results.php
+++ src/core/race/partials/list.race.results.php
@@ -5,14 +5,16 @@ echo '<div class="container_wrap">
 
 foreach($events as $event) :
     echo '<div class="row row-striped" id="'.$event->ID.'">';
-        echo sprintf('<div>Event %s</div>',$event->eventname);
-    if($event->racecount==1){
-        echo '<div>Race <a href="'.get_permalink($event->race1).'">'.get_the_title($event->race1).'</a></div>';
-    }   else {
-        echo '<div>Races <a href="'.get_permalink($event->race1).'">'.get_the_title($event->race1).' - </a>';
-        echo '<a href="'.get_permalink($event->race2).'">'.get_the_title($event->race2).'</a></div>';
-    }
-    echo '<div>League <a href="'.get_permalink($event->league).'">'.get_the_title($event->league).'</a></div>';
+    echo sprintf('<div class="col-sm-4">Event %s</div>',$event->eventname);
+        if($event->racecount==1){
+            echo '<div class="col-sm-8">Race <a href="'.get_permalink($event->race1).'">'.get_the_title($event->race1).'</a></div>';
+        }   else {
+            echo '<div class="col-sm-4">Races <a href="'.get_permalink($event->race1).'">'.get_the_title($event->race1).'</a></div>';
+            echo '<div class="col-sm-4"><a href="'.get_permalink($event->race2).'">'.get_the_title($event->race2).'</a></div>';
+        }
+    echo '</div>';
+    echo '<div class="row row-striped row-minor">';
+        echo '<div class="col-sm-4">League</div><div class="col-sm-8"><a href="'.get_permalink($event->league).'">'.get_the_title($event->league).'</a></div>';
     echo '</div>';
 endforeach;
 
