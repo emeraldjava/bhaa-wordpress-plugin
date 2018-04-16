05063ffe2aa6eee289c60814e8db57d9f441ad93
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-16 17:36:18 +0100

    Update deploy.sh list.

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ echo 'FTP_SITE     '$3
 echo 'FTP_PATH     '$4
 echo ftp://$1:$2@$3/$4
 curl --ftp-create-dirs -u "$1:$2" -T README.md                               ftp://$3/$4/README.md
+curl --ftp-create-dirs -u "$1:$2" -T CHANGEME.md                             ftp://$3/$4/CHANGEME.md
 curl --list-only -u "$1:$2" ftp://$3/$4/
