4bae64a484b70bd49bd90f13130eb47dd6a49e11
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-04-20 16:04:32 +0100

    2018.04.20 Ensure we report all race results by position.

--- src/core/race/RaceResult.php
+++ src/core/race/RaceResult.php
@@ -152,13 +152,15 @@ class RaceResult {
         $query = "SELECT wp_bhaa_raceresult.*,
                 first_name.meta_value AS firstname, 
                 UPPER(last_name.meta_value) AS surname,
-                wp_users.user_nicename,gender.meta_value as gender,
-                wp_posts.id as cid,wp_posts.post_title as cname,
+                wp_users.user_nicename,
+                gender.meta_value as gender,
+                wp_posts.id as cid,
+                wp_posts.post_title as cname,
                 IF(status.meta_value='M',true,false) as isMember
                 FROM wp_bhaa_raceresult
                 left join wp_users on wp_users.id=wp_bhaa_raceresult.runner
-                join wp_usermeta first_name on (first_name.user_id=wp_users.id and first_name.meta_key='first_name')
-                join wp_usermeta last_name on (last_name.user_id=wp_users.id and last_name.meta_key='last_name')
+                left join wp_usermeta first_name on (first_name.user_id=wp_users.id and first_name.meta_key='first_name')
+                left join wp_usermeta last_name on (last_name.user_id=wp_users.id and last_name.meta_key='last_name')
                 left join wp_usermeta gender on (gender.user_id=wp_users.id and gender.meta_key='bhaa_runner_gender')
                 left join wp_usermeta company on (company.user_id=wp_users.id and company.meta_key='bhaa_runner_company')
                 left join wp_usermeta status on (status.user_id=wp_users.id and status.meta_key='bhaa_runner_status')
