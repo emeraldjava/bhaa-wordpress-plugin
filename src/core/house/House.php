f6496a48a93155f4c4493a93b210eaed4e092ce4
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-24 21:12:50 +0100

    2018.04.24 Add TeamResult to deploy.sh script

---
+++ src/core/house/House.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace BHAA\core\house;
+
+use BHAA\core\Connections;
+
+class House {
+    const HOUSE = 'house';
+    const SECTOR = 'sector';
+
+    const TEAM_TYPE = 'teamtype';
+    const COMPANY_TEAM = 'company';
+    const SECTOR_TEAM = 'sector';
+
+    const TEAM_STATUS = 'teamstatus';
+    const ACTIVE = 'ACTIVE';
+    const PENDING = 'PENDING';
+
+    var $houseid;
+    var $teamType;
+    var $connectionType;
+
+    function __construct($houseid) {
+        $this->houseid = $houseid;
+        $terms = wp_get_post_terms($this->houseid,House::TEAM_TYPE);
+        $this->teamType=isset($terms[0]->name)?$terms[0]->name:House::COMPANY_TEAM;
+        $this->connectionType = Connections::HOUSE_TO_RUNNER;
+        if($this->teamType==House::SECTOR_TEAM) {
+            $connected_type = Connections::SECTORTEAM_TO_RUNNER;
+        }
+    }
+
+    function getTeamType(){
+        $this->teamType;
+    }
+
+    function getConnectionType(){
+        return $this->connectionType;
+    }
+
+    function getLinkedRunners() {
+        $users = get_users( array(
+            'connected_type' => $this->getConnectionType(),
+            'connected_items' => $this->houseid,
+            'fields' => 'all',
+            'orderby' => 'display_name',
+            'order' => 'ASC'
+        ));
+        //var_dump($users[0],true);
+
+        $runners = array();
+        foreach ( $users AS $user ) {
+            $runners[] = new Runner($user->ID);
+        }
+
+        $x = array_values($runners);
+        //var_dump($x[0]);
+        return $x;
+    }
+
+    function getLinkedRunners2(){
+        $usersByCompanyQuery = new WP_User_Query(
+            array(
+                'fields' => 'all_with_meta',
+                'orderby' => 'bhaa_runner_dateofrenewal',
+                'query_id'=>'get_bhaa_meta',
+                'meta_query' => array(
+                    array(
+                        'key' => 'bhaa_runner_company',
+                        'value' => $this->houseid,
+                        'compare' => '=')
+                )
+            )
+        );
+        $users = $usersByCompanyQuery->get_results();
+        //var_dump($users);
+        //$x = array_values($users);
+        //var_dump($x[0]);
+        return $x;//users;
+    }
+
+
+
+    function house_website_url(){
+        $site = get_post_meta(get_the_ID(),'bhaa_company_website',true);
+        if(!empty($site))
+            return 'Visit the '.get_the_title().' website : <a target="new" href="'.$site.'">'.$site.'</a>';
+    }
+
+    /**
+     * Use the template 'house-runner' to show the runners linked to a house.
+     * @return string
+     */
+    function displayRunnersTable() {
+        return Bhaa_Mustache::get_instance()->loadTemplate('house-runners')->render(
+            array('users' => $this->getLinkedRunners()
+            ));
+    }
+
+    //add_action( 'pre_user_query', 'wps_pre_user_query' );
+    /*
+     * Modify the WP_User_Query appropriately
+    *
+    * Checks for the proper query to modify and changes the default user_login for $wpdb->usermeta.meta_value
+    *
+    * @param WP_User_Query Object $query User Query object before query is executed
+    */
+    // http://stackoverflow.com/questions/12392847/wordpress-user-query-order-by-meta-value
+    //function wps_pre_user_query( &$query ) {
+    //	global $wpdb;
+    //	if ( isset( $query->query_vars['query_id'] ) && 'wps_last_name' == $query->query_vars['query_id'] )
+    //		$query->query_orderby = str_replace( 'user_login', "$wpdb->usermeta.meta_value", $query->query_orderby );
+    //}
+
+    function getRunners(){
+        $usersByCompanyArgs = new WP_User_Query(
+            array(
+                //'exclude' => array($user->ID),
+                'fields' => 'all',//_with_meta',
+                'orderby' => 'display_name',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    //'relation' => 'AND',
+                    array(
+                        'key' => 'bhaa_runner_company',
+                        'value' => $this->houseid,
+                        'compare' => '=')
+                    //array(
+//										'key' => 'bhaa_runner_dateofrenewal',
+                    //									'value' => '2014',
+                    //								'compare' => 'LIKE',
+                    //'type'=>'DATE')
+                )
+            )
+        );
+        //var_dump($usersByCompanyArgs);
+        //$userQuery = new WP_User_Query( $usersByCompanyArgs );
+
+//		var_dump($usersByCompanyArgs);
+
+        return $usersByCompanyArgs->get_results();
+    }
+}
+?>
