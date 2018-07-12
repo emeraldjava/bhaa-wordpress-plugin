6b127718e9fa389dfa70fe705f883eef1670ff15
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-07-12 16:16:27 +0100

    2018.07.12 Use wp_get_archives().

--- src/core/results/partials/results.by.year.php
+++ src/core/results/partials/results.by.year.php
@@ -1,9 +1,23 @@
 <?php
 get_header();
+// https://digwp.com/2010/10/dynamic-archives/
 ?>
 <div class="container_wrap">
-    <h2>Results by Year 2018</h2>
-    <div class="container">';
+    <h3>BHAA Results by Year <?php echo $year ?>.</h3>
+
+    <?php wp_get_archives(array('type'=>'yearly','format'=>'html','before'=>'/results','post_type'=>'race'));?>
+
+    <hr/>
+
+
+    <div class="container">
+        <form id="year_form" method="POST" action="">
+            <select name="year" id="year">
+                <?php wp_get_archives(array('type'=>'yearly','format'=>'option','post_type'=>'race'));?>
+            </select>
+            <input type="submit" name="Search"/>
+        </form>
+        <hr/>
         <div class="col-8">Races<?php echo do_shortcode('[bhaa_races_by_year]');?></div>
         <div class="col-4">Leagues<?php echo do_shortcode('[bhaa_leagues_by_year]');?></div>
     </div>
