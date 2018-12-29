c2978d5ca6263c412da28e1434608f1b6cc0118b
emeraldjava <paul.t.oconnell@gmail.com>
2018-12-29 11:09:40 +0000

    2018.12.29 Capture custom question answers as runner meta-data.

--- bhaa_wordpress_plugin.php
+++ bhaa_wordpress_plugin.php
@@ -6,7 +6,7 @@
  * Plugin Name:       BHAA Wordpress Plugin
  * Plugin URI:        https://github.com/emeraldjava/bhaa_wordpress_plugin
  * Description:       Plugin for the Business House Athletic Association. Handles user registration, race results and leagues.
- * Version:           2018.12.23
+ * Version:           2018.12.29
  * Author:            emeraldjava
  * Author URI:        https://github.com/emeraldjava
  * License:           GPL-2.0+
