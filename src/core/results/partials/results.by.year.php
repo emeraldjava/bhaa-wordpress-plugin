f32403234bb2ced135cb211c94bd4ef800b86fa4
emeraldjava <paul.t.oconnell@gmail.com>
2019-01-16 22:24:41 +0000

    2019.01.16 Update the results layout.

--- src/core/results/partials/results.by.year.php
+++ src/core/results/partials/results.by.year.php
@@ -5,14 +5,12 @@ get_header();
 <div class="container-fluid">
     <h3>BHAA Results by Year <?php echo $year ?></h3>
     <div class="row">
-        <div class="col-6">Races:</div>
-        <div class="col-4">Leagues:</div>
-        <div class="col-2">Years:</div>
+        <div class="col-8">Races:</div>
+        <div class="col-4">Years:</div>
     </div>
     <div class="row">
-        <div class="col-6"><?php echo $racesByYearList; ?></div>
-        <div class="col-4"><?php echo $leaguesByYearList;?></div>
-        <div class="col-2"><?php echo $years; ?></div>
+        <div class="col-8"><?php echo $racesByYearList; ?></div>
+        <div class="col-4"><?php echo $years; ?></div>
     </div>
 </div>
 <?php
