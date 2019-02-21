d9426d3dea00c26e6556928fc60da95577372f56
emeraldjava <paul.t.oconnell@gmail.com>
2019-02-21 22:17:02 +0000

    2019.02.21 Restore race template.

--- src/core/cpt/partials/race/race.php
+++ src/core/cpt/partials/race/race.php
@@ -1,26 +1,16 @@
 <?php
 get_header();
-$results = get_query_var('raceResultTable');
-$teams = get_query_var('teamResultTable');
-echo sprintf('
-<div class="container_wrap">
+echo '<div class="container_wrap">
     <div class="container">
-        <div class="content">
-        <!-- https://www.quackit.com/bootstrap/bootstrap_4/tutorial/bootstrap_navs.cfm -->
-        <ul class="nav nav-tabs">
-          <li class="nav-item"><a class="nav-link active" role="tab" data-toggle="tab" href="#results">Results</a></li>
-          <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#teams">Teams</a></li>
-          <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#awards">Awards</a></li>
-          <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#standards">Standards</a></li>
-        </ul>   
-        <div class="tab-content">
-          <div id="results" class="tab-pane fade show active" role="tabpanel" aria-labelledby="home-tab">%s</div>
-          <div id="teams" cclass="tab-pane fade show" role="tabpanel">%s</div>
-          <div id="awards" cclass="tab-pane fade show" role="tabpanel">Awards</div>
-          <div id="standards" cclass="tab-pane fade show" role="tabpanel">Standards</div>
-        </div>
-        </div>
-    </div>
-</div>',$results,$teams);
+    <div class="content">';
+//echo do_shortcode('[bhaa_race_title]');
+
+echo '<h2>Individual Results</h2>';
+echo get_query_var('raceResultTable');
+
+echo '<h2>Team Results</h2>';
+echo get_query_var('teamResultTable');
+
+echo '</div></div></div>';
 get_footer();
 ?>
