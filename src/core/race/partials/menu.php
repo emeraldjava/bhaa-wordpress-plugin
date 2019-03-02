1bf0bba83d2dad76df558e240b70d46756bcedde
emeraldjava <paul.t.oconnell@gmail.com>
2019-03-02 22:40:28 +0000

    2019.03.02 Use permalink for race href.

--- src/core/race/partials/menu.php
+++ src/core/race/partials/menu.php
@@ -1,12 +1,10 @@
 <ul class="nav">
 <?php
-if(is_admin()){
-    echo '<li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/admin">Admin</a></li>';
-  }
+$race_url = get_permalink(get_the_ID());
 ?>
-  <li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/overview">Overview</a></li>
-  <li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/results">Results</a></li>
-  <li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/teams">Teams</a></li>
-  <li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/awards">Awards</a></li>
-  <li class="nav-item"><a class="nav-link" href="http://localhost/race/garda-4m-2019/standards">Standards</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?php echo $race_url;?>/overview">Overview</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?php echo $race_url;?>/results">Results</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?php echo $race_url;?>/teams">Teams</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?php echo $race_url;?>/awards">Awards</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?php echo $race_url;?>/standards">Standards</a></li>
 </ul>
