2ec02d0ac841b55b1a995dd3ebb39c63197d78c8
emeraldjava <paul.t.oconnell@gmail.com>
2019-03-19 16:14:16 +0000

    2019.03.19 Use rows and cards for awards layout.

--- src/core/race/RaceAwardsView.php
+++ src/core/race/RaceAwardsView.php
@@ -14,32 +14,93 @@ class RaceAwardsView {
     function generateView($awards) {
         $awardsReport = '';
         $currentAgeGroup = null;
+        $awardsReport .= '<div id="container">';
+
         foreach($awards as $award) {
-            $awardsReport .= '<div id="accordion">';
             if($currentAgeGroup!=$award->agegroup) {
                 $currentAgeGroup = $award->agegroup;
                 $awardsReport .= $this->generateAgeCategoryCard($award->agegroup);
             }
-            $awardsReport .= '</div>';
         }
+        $awardsReport .= '</div>';
         return $awardsReport;
     }
 
     function generateAgeCategoryCard($ageCategory) {
-        return sprintf('<div class="card">
-            <div class="card-header" id="%1$s">
-              <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapse%1$s" aria-expanded="true" aria-controls="collapseOne">
-                  AgeGroup %1$s
-                </button>
-              </h5>
+        return sprintf(
+            '<div class="row">
+                <div class="col-md-12 col-lg-12 col-xl-12 card-header" id="%1$s">
+                  <div class="card">
+                    <div class="card-block">
+                      <h4 class="card-title"><span>AgeGroup</span> <span>%1$s</span></h4>
+                    </div>
+                  </div>  
+                </div>
             </div>
-        
-            <div id="collapse1%s" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-              <div class="card-body">
-                The %1$s details
-              </div>
+            
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-xl-6 card-header" id="%1$s_M">
+                    <div class="card">
+                        <div class="card-block">
+                          <h5 class="card-title">Men</h5>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6 col-xl-6 card-header" id="%1$s_W">
+                    <div class="card">
+                        <div class="card-block">
+                          <h5 class="card-title">Women</h5>
+                        </div>  
+                    </div>
+                </div>
             </div>
+             
+            <div class="row">
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p1m">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P1 Male</h6>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p2m">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P2 Male</h6>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p3m">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P3 Male</h6>
+                        </div>  
+                    </div>
+                </div>
+                
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p1w">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P1 Woman</h6>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p2w">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P2 Woman</h6>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 card-header" id="%1$s_p3w">
+                    <div class="card">
+                        <div class="card-block">
+                          <h6 class="card-title">P3 Woman</h6>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            
           </div>',$ageCategory);
     }
 
