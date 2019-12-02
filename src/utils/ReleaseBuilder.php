1fdc99b2d419b3bc369b4bfb25b2fa4dd7511554
emeraldjava <paul.t.oconnell@gmail.com>
2019-12-02 16:40:49 +0000

    2019.12.02 Add composer scripts to create a release zip file during the post install phase.

---
+++ src/utils/ReleaseBuilder.php
@@ -0,0 +1,55 @@
+<?php
+namespace BHAA\utils;
+
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use RecursiveCallbackFilterIterator;
+use ZipArchive;
+
+// https://stackoverflow.com/questions/4914750/how-to-zip-a-whole-folder-using-php
+class ReleaseBuilder {
+
+    public static function createZip() {
+        //printf("ReleaseBuilder ".dirname(__FILE__).PHP_EOL);
+        //printf("basename ".basename(__DIR__).PHP_EOL);
+        $zip = new ZipArchive;
+        $zip->open('bhaa_wordpress_plugin.zip', ZipArchive::CREATE);
+
+        $rootPath = realpath(__DIR__ . '/../..');
+        //printf("rootPath ".$rootPath.PHP_EOL);
+
+        $exclude = array('.git','.github','.idea','tests','docs');
+        $filter = function ($file, $key, $iterator) use ($exclude) {
+            if ($iterator->hasChildren() && !in_array($file->getFilename(), $exclude)) {
+                return true;
+            }
+            return $file->isFile();
+        };
+
+        $innerIterator = new RecursiveDirectoryIterator(
+            $rootPath,
+            RecursiveDirectoryIterator::SKIP_DOTS
+        );
+        $files = new RecursiveIteratorIterator(
+            new RecursiveCallbackFilterIterator($innerIterator, $filter)
+        );
+
+        foreach ($files as $name => $file)
+        {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir())
+            {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+    }
+}
+?>
